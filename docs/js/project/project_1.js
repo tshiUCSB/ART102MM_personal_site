cd4eed6d2ff85167782bd9c87a3cbd2294e34a46
Tina Shi <tshi@umail.ucsb.edu>
2020-10-11 11:58:40 -0700

    restyled to populate projects w/ js and started project_1

---
+++ docs/js/project/project_1.js
@@ -0,0 +1,20 @@
+/*
+Project 1 for ART102MM
+10/11/20
+Code by Tina Shi
+URL: https://tshiucsb.github.io/ART102MM_personal_site/html/project/project_1.html
+*/
+
+
+function setup() {
+	let cnv = createCanvas(window.innerWidth / 2, 
+		window.innerWidth / 2 / 475 * 600);
+	background("#fff");
+	cnv.position(window.innerWidth / 2, 0, "fixed");
+}
+
+function draw() {
+
+}
+
+
