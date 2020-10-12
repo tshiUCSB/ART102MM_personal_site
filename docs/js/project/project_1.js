ea3bde285417a1311f05df1b0ca37cf1d651d93d
Tina Shi <tshi@umail.ucsb.edu>
2020-10-12 01:57:58 -0700

    positioned all shapes

--- docs/js/project/project_1.js
+++ docs/js/project/project_1.js
@@ -6,7 +6,8 @@ URL: https://tshiucsb.github.io/ART102MM_personal_site/html/project/project_1.ht
 */
 
 var colors = null;
-var kandinsky_shapes = null;
+var pre_shapes = null;
+var post_shapes = null;
 function Shape(type, x, y, w, h, fill_color, strk_color=null, strk_weight=null, blend=null) {
 	this.type = type;
 	this.x = Math.floor(x);
@@ -16,10 +17,11 @@ function Shape(type, x, y, w, h, fill_color, strk_color=null, strk_weight=null, 
 	this.fill_color = fill_color;
 	this.strk_color = strk_color;
 	this.strk_weight = Math.floor(strk_weight);
+	this.blend = blend;
 }
 
 function init_shapes() {
-	let k = {
+	pre_shapes = {
 		n_w_circle_br_outln: new Shape("circle", width * .1, height * .1,
 			width * .12, null, colors.br, color(100, 80, 100), width * .03),
 
@@ -102,12 +104,73 @@ function init_shapes() {
 			width * .05, null, colors.g, color(120, 120, 120), width * .002),
 
 		s_w_circle_w: new Shape("circle", width * .13, height * .91, 
-			width * .03, null, color(230, 230, 240)),
+			width * .03, null, colors.w),
 		
 		w_square_bg: new Shape("square", width * .47, height * .95 / 2, 
 			width * .3, null, colors.bg)
 	};
-	kandinsky_shapes = k;
+
+	post_shapes = {
+		n_w_circle_g: new Shape("circle", width * .4, height * .37, 
+			width * .05, null, color(80, 100, 80)),
+
+		n_w_circle_w_m: new Shape("circle", width * .47, height * .45, 
+			width * .08, null, colors.w),
+
+		n_w_circle_blk_s: new Shape("circle", width * .47, height * .455, 
+			width * .015, null, colors.blk),
+
+		n_w_circle_grey_m: new Shape("circle", width * .41, height * .456, 
+			width * .07, null, color(180, 180, 180), null, null, "MULTIPLY"),
+
+		w_circle_dkBl: new Shape("circle", width * .33, height * .45, 
+			width * .1, null, color(50, 60, 100)),
+
+		n_circle_r_burn: new Shape("circle", width * .5, height * .43, 
+					width * .06, null, colors.r, null, null, "BURN"),
+
+		n_circle_blk_m: new Shape("circle", width * .55, height * .43, 
+			width * .075, null, colors.blk, color.blk, 1),
+
+		n_circle_r_screen: new Shape("circle", width * .5, height * .43, 
+			width * .06, null, colors.r, null, null, "SCREEN"),
+
+		circle_br: new Shape("circle", width * .54, height * .48, 
+			width * .03, null, color(50, 30, 30)),
+
+		s_circle_g: new Shape("circle", width * .59, height * .49, 
+			width * .03, null, colors.g),
+
+		s_circle_y: new Shape("circle", width * .6, height * .57, 
+			width * .025, null, color(150, 150, 50)),
+
+		n_w_circle_blk_m: new Shape("circle", width * .43, height * .41, 
+			width * .075, null, colors.blk),
+
+		n_w_circle_w_s: new Shape("circle", width * .43, height * .39, 
+			width * .015, null, colors.w),
+
+		n_w_circle_warm_grey: new Shape("circle", width * .41, height * .41, 
+			width * .025, null, color(150, 150, 130)),
+
+		n_w_circle_grey_s: new Shape("circle", width * .445, height * .41, 
+			width * .02, null, color(100, 100, 100)),
+
+		n_w_circle_br: new Shape("circle", width * .47, height * .39, 
+			width * .04, null, color(80, 30, 30), colors.blk, 1, "SCREEN"),
+
+		s_w_circle_dkBr: new Shape("circle", width * .46, height * .53, 
+			width * .15, null, color(35, 10, 30)),
+
+		s_w_circle_ice: new Shape("circle", width * .36, height * .52, 
+			width * .03, null, color(150, 200, 220)),
+
+		s_w_circle_blk_xs: new Shape("circle", width * .35, height * .57, 
+			width * .025, null, colors.blk),
+
+		s_w_circle_blk_s: new Shape("circle", width * .4, height * .5, 
+			width * .04, null, colors.blk)
+	}
 }
 
 function draw_background() {
@@ -116,12 +179,26 @@ function draw_background() {
 	fill(colors.bg_quad);
 	quad(Math.floor(width / 3), 0, width, Math.floor(height / 3), 
 		Math.floor(width / 3 * 2), height, 0, Math.floor(height * .55));
-	fill(colors.bg);
+	
+	// let slope_1 = height * .45 / (width / 3 * 2);
+	// let slope_2 = -height / 3 * 2 / (width / 3);
+	// triangle(width * .2, width * .2 * slope_1 + height * .55, );
+
+	fill(colors.bg_dark);
 	beginShape();
 	vertex(0, 0);
 	vertex(Math.floor(width / 3), 0);
 	let v = [{x: Math.floor(width * .2), y: Math.floor(height * .2)}, 
 		{x: Math.floor(width * .25), y: width * .4}, {x: 0, y: Math.floor(height * .55)}];
+	bezierVertex(v[0].x, v[0].y, v[1].x, v[1].y, v[2].x, v[2].y);
+	endShape();
+
+	beginShape();
+	vertex(Math.floor(width / 3), 0);
+	vertex(width, 0);
+	vertex(width, height / 3);
+	v = [{x: Math.floor(width * .8), y: Math.floor(height * .28)}, 
+		{x: Math.floor(width * .55), y: Math.floor(height * .2)}, {x: Math.floor(width / 3), y: 0}];
 	bezierVertex(v[0].x, v[0].y, v[1].x, v[1].y, v[2].x, v[2].y);
 	endShape();
 }
@@ -135,6 +212,7 @@ function draw_shapes(shapes) {
 		else {
 			noFill();
 		}
+
 		if (s.strk_color) {
 			stroke(s.strk_color);
 			if (s.strk_weight) {
@@ -144,6 +222,24 @@ function draw_shapes(shapes) {
 		else {
 			noStroke();
 		}
+
+		if (s.blend == "OVERLAY") {
+			blendMode(OVERLAY);
+		}
+		else if (s.blend == "SCREEN") {
+			blendMode(SCREEN);
+		}
+		else if (s.blend == "BURN") {
+			blendMode(BURN);
+		}
+		else if (s.blend == "DODGE") {
+			blendMode(DODGE);
+		}
+		else if (s.blend == "MULTIPLY") {
+			blendMode(MULTIPLY);
+		}
+
+
 		if (s.type == 'circle') {
 			ellipse(s.x, s.y, s.w);
 		}
@@ -151,6 +247,7 @@ function draw_shapes(shapes) {
 			rectMode(CENTER);
 			square(s.x, s.y, s.w);
 		}
+		blendMode(BLEND);
 	}
 }
 
@@ -158,6 +255,7 @@ function setup() {
 	colors = {
 		bg: color(40, 50, 60),
 		bg_quad: color(220, 160, 80),
+		bg_dark: color(20, 30, 40),
 		r: color(200, 50, 50),
 		br: color(80, 50, 50),
 		blk: color(0, 0, 0),
@@ -165,7 +263,8 @@ function setup() {
 		lt_bl: color(50, 100, 120),
 		bl: color(80, 100, 120),
 		g: color(45, 80, 50),
-		pale_pink: color(180, 120, 120, 150)
+		pale_pink: color(180, 120, 120, 150),
+		w: color(230, 230, 240)
 	};
 	let cnv = createCanvas(window.innerWidth / 2, 
 		window.innerWidth / 2 / 475 * 600);
@@ -175,7 +274,8 @@ function setup() {
 
 function draw() {
 	draw_background();
-	draw_shapes(kandinsky_shapes);
+	draw_shapes(pre_shapes);
+	draw_shapes(post_shapes);
 }
 
 
