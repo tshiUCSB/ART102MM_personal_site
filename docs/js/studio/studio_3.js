241b3a2ec0db1ee20fafe6504cd077b60f4acec1
Tina Shi <tshi@umail.ucsb.edu>
2020-10-15 04:22:53 -0700

    commented studio 3

--- docs/js/studio/studio_3.js
+++ docs/js/studio/studio_3.js
@@ -9,36 +9,49 @@ var cube = null;
 var bg_color = "#000";
 
 function preload() {
-	cube = {};
-	cube["src"] = loadImage("../../assets/cube_icon.png");
+	// loads in image for cube
+	cube = loadImage("../../assets/cube_icon.png");
 }
 
 function setup() {
+	// create canvas to fill window
 	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+	// fill background with specified background color
 	background(bg_color);
+	// draw image by specifying coordinate for center of image
 	imageMode(CENTER);
+	// cube width takes up 20% of canvas width
 	let cube_w = width * .2;
+	// cube height depends on cube width for set asepct ratio
 	let cube_h = cube_w / 930 * 989;
 
+	// save and stash current settings
 	push()
+	// translate canvas origin to center
 	translate(width / 2, height / 2);
+	// rotate canvas around origin by amount of phone rotation in Z
 	rotate(rotationZ);
-	image(cube.src, 0, 0, cube_w, cube_h);
+	// draw cube to canvas at origin
+	image(cube, 0, 0, cube_w, cube_h);
+	// resume saved settings
 	pop()
 }
 
 function touchStarted() {
+	// if phone screen is touched, change background color to white
 	bg_color = "#fff";
 }
 
 function touchEnded() {
+	// if phone screen is no longer touched, change background color back to black
 	bg_color = "#000";
 }
 
 function windowResized() {
+	// if window resized, resize canvas to fill window
 	resizeCanvas(windowWidth, windowHeight);
 }
 
