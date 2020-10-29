64feeea45b8b1964c74170018c75d03bacdf3639
Tina Shi <tshi@umail.ucsb.edu>
2020-10-29 03:06:59 -0700

    implemented project 3

--- docs/js/project/project_3.js
+++ docs/js/project/project_3.js
@@ -14,8 +14,13 @@ var MSG_INPUT = null;
 var DESCRIPTION = null;
 var msg_content = "";
 
+var has_typed = false;
+var total_count = 0;
+var recover_count = 0;
+
 var particles = null;
-function Particle(x, y, z, r, nx, ny, nz, vx, vy, vz, c={r: 0, g: 0, b: 0}, is_infected=true, is_infectious=false) {
+function Particle(x, y, z, r, nx, ny, nz, vx, vy, vz, rx, ry, rz, 
+	c={r: 0, g: 0, b: 0}, is_infected=true, is_infectious=false) {
 	this.x = x;
 	this.y = y;
 	this.z = z;
@@ -26,6 +31,9 @@ function Particle(x, y, z, r, nx, ny, nz, vx, vy, vz, c={r: 0, g: 0, b: 0}, is_i
 	this.vx = vx;
 	this.vy = vy;
 	this.vz = vz;
+	this.rx = rx;
+	this.ry = ry;
+	this.rz = rz;
 	this.c = c;
 	this.is_infected = is_infected;
 	this.is_infectious = is_infectious;
@@ -39,17 +47,28 @@ function Particle(x, y, z, r, nx, ny, nz, vx, vy, vz, c={r: 0, g: 0, b: 0}, is_i
 	}
 	this.draw = function() {
 		push();
-		ambientLight(100);
-		emissiveMaterial(color(this.c.r, this.c.g, this.c.b));
-		// normalMaterial();
 		translate(this.x, this.y, this.z);
-		sphere(this.r);
+		rotateX(frameCount * this.rx);
+		rotateY(frameCount * this.ry);
+		rotateZ(frameCount * this.rz);
+		ambientLight(250);
+		pointLight(215, 19, 67, mouseX - width / 2, mouseY - height / 2, 50);
+		specularMaterial(this.c.r, this.c.g, this.c.b);
+		shininess(20);
+		box(this.r);
 		pop();
 		this.calculate_pos();
 	}
 	this.connect = function(p, threshold) {
 		if (p != this && dist(this.x, this.y, this.z, p.x, p.y, p.z) < threshold) {
 			line(this.x, this.y, this.z, p.x, p.y, p.z);
+			return true;
+		}
+		return false;
+	}
+	this.wither = function(rate) {
+		this.r -= rate;
+		if (this.r <= 0) {
 			return true;
 		}
 		return false;
@@ -62,24 +81,41 @@ function setup() {
 	particles = {};
 	for(let i = 0; i < floor(TOTAL_CONFIMRED / 100); i++) {
 		particles[i.toString()] = new Particle(width / 2 * random(-1, 1), 
-			width / 2 * random(-1, 1), height / 2 * random(-1, 1), random() * 10 + 2, 
+			width / 2 * random(-1, 1), height / 2 * random(-1, 1), random() * 20 + 5, 
 			random(.0001, .0003), random(.0001, .0003), random(.0001, .0003), 
-			random(width), random(width), random(height), undefined, undefined, 
-			random() > INFECTIOUS / TOTAL_CONFIMRED ? false : true);
+			random(width), random(width), random(height), 
+			random(-.01, .01), random(-.01, .01), random(-.01, .01), undefined, undefined, 
+			random() > INFECTIOUS / TOTAL_CONFIMRED * 2 ? false : true);
 	}
+	total_count = Object.keys(particles).length;
 }
 
 function draw() {
-	background("#fff");
+	if (has_typed)
+		background(lerpColor(color(0, 0, 0), color(255, 255, 255), recover_count / total_count));
+	else clear();
 	for(const key in particles) {
 		let p = particles[key];
 		p.draw();
 		if (p.is_infectious) {
 			for(const i in particles) {
 				let connected = p.connect(particles[i], 6 * 12);
-				if (connected) particles[i].is_infected = true;
+				if (connected && has_typed) {
+					particles[i].is_infected = true;
+					particles[i].is_infectious = true;
+					particles[i].c = {r: 0, g: 0, b: 0};
+				}
 			}
 		}
+	}
+	let rm_keys = [];
+	recover_count = 0;
+	for(const key in particles) {
+		if (!particles[key].is_infected) recover_count++;
+		else if (has_typed && particles[key].wither(.00001)) rm_keys.push(key);
+	}
+	for(let i = 0; i < rm_keys.length; i++) {
+		delete particles[rm_keys[i]];
 	}
 }
 
@@ -108,6 +144,7 @@ function parse_str_into_color(str) {
 
 function handle_new_input(e) {
 	let msg_change = e.target.value
+	if (!has_typed) has_typed = true;
 	if (msg_change.endsWith(' ') || msg_change.endsWith('\n')) {
 		msg_content += msg_change;
 		DESCRIPTION.innerHTML = msg_content;
