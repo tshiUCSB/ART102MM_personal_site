09f128783cb706d7d9a11f2d672b719b771b2c4a
Tina Shi <tshi@umail.ucsb.edu>
2020-11-02 18:33:05 -0800

    commented studio 8

--- docs/js/studio/studio_8.js
+++ docs/js/studio/studio_8.js
@@ -12,40 +12,51 @@ var permissionGranted = false;
 var deviceX = undefined;
 var deviceY = undefined;
 
+// check permission for device orientation event
 function check_permission() {
 	if (typeof(window.DeviceOrientationEvent) !== 'undefined' 
 		&& typeof(window.DeviceOrientationEvent.requestPermission) === 'function') {
+		// if support device orientation, create button for user to grant permission
 		button = createButton("Allow access to sensors");
 		button.style("font-size", "24px");
 		button.center();
 		button.mousePressed(requestAccess);
 	}
 	else {
+		// if no support, draw text to canvas
 		textSize(24);
 		text("This device doesn't support device orientation", 100, 100);
 	}
 }
 
 function setup() {
+	// create canvas to fill window
 	createCanvas(windowWidth, windowHeight);
+	// create sine wave oscillator
 	osc = new p5.SinOsc();
 	osc.amp(.5);
+	// initialize initial position
 	deviceX = width / 2;
 	deviceY = height / 2;
+	// check for device orientation permission
 	check_permission();
 }
 
 function draw() {
 	if (!permissionGranted) return;
+	// update position based rotation
 	deviceX -= constrain(rotationX, -3, 3);
 	deviceY += constrain(rotationY, -3, 3);
 
+	// constrain position within canvas
 	deviceX = constrain(deviceX, 0, width);
 	deviceY = constrain(deviceY, 0, height);
 
+	// draw ellipse at position
 	ellipse(deviceX, deviceY, 100, 100);
 
 	if (play) {
+		// play sound through sine wave oscillator
 		let freq = map(deviceX, 0, width, 40.88);
 		osc.freq(freq);
 
@@ -55,21 +66,25 @@ function draw() {
 }
 
 function touchStarted() {
+	// allow play sound on touch
 	play = true;
 	osc.start();
 }
 
 function touchEnded() {
+	// stop playing sound when touch disengaged
 	play = false;
 	osc.stop();
 }
 
 function requestAccess() {
+	// set permission state based on user selection
 	DeviceOrientationEvent.requestPermission().then(response => {
 		if (response === 'granted') {
 			permissionGranted = true;
 		}
 	}).catch(console.error);
+	// remove button
 	button.remove();
 }
 
